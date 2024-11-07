4230cdc6a1f12443cf50be15c484dc0b50e0b3d7
tesar-tech <jan@tesar.tech>
2024-11-07 18:27:41 +0000

    0

--- run_the_template.bash
+++ run_the_template.bash
@@ -2,13 +2,13 @@
 
 # Define the directory name
 # dir="BlazoriseBugReport_$(date +"%y%m%d")"
-dir = "BlazoriseBugReport"
+template_dir = "BlazoriseBugReport"
 
 dotnet new uninstall Blazorise.Templates
 dotnet new install Blazorise.Templates::1.6.0
 
 # Check if the folder exists in the current directory and remove it if it does
-[ -d "$dir" ] && rm -rf "$dir"
+[ -d "$template_dir" ] && rm -rf "$template_dir"
 
-dotnet new blazorise -n "$dir" -p Bootstrap5 -bh WebApp  -ut true -f net8.0
+dotnet new blazorise -n "$template_dir" -p Bootstrap5 -bh WebApp  -ut true -f net8.0
 
